9e2aecb5d0c1b32bb2325ab078f91d890076cc4b
Daniel Searle <dan@d-searle.co.uk>
2010-02-28 17:40:09 +0000

    Modified the mmap syscall number to be mmap2

--- lib/LinuxMacros.asm
+++ lib/LinuxMacros.asm
@@ -26,7 +26,7 @@
 %define sys_exit  0x01 ; sys_exit function number see `man 2 exit`
 %define sys_read  0x03 ; sys_read function number see `man 2 read`
 %define sys_write 0x04 ; sys_write function number see `man 2 write`
-%define sys_mmap  0x5A ; sys_mmap function number see `man 2 mmap`
+%define sys_mmap2 0xC0 ; sys_mmap2 function number see `man 2 mmap2`
 ; Files --------------------------------------------------------------------------
 ;               File descriptors to use.                                          `
 %define STDIN     0x00 ; Standard input  buffer is 0, when using sys_* functions.
